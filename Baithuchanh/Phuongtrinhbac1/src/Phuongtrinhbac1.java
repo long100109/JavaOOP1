ebd5f91fae14cd45d6d4643d3947a8def51a3f70
long100109 <Ngolong2010@gmail.com>
2024-01-25 21:13:48 +0700

    Baithuchanh

---
+++ Baithuchanh/Phuongtrinhbac1/src/Phuongtrinhbac1.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class Phuongtrinhbac1{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        double a, b, x;
+        System.out.println(" Nhap a: ");
+        a= sc.nextDouble();
+        System.out.println(" Nhap b: ");
+        b= sc.nextDouble();
+        sc.close();
+        if(a == 0) {
+            if(b == 0) {
+                System.out.println(" PT co vo so nghiem");
+            } 
+            else{
+                System.out.println(" PT vo nghiem");
+            }
+        } 
+        else{
+            x=-b/a;
+            System.out.println("x= "+x);
+        }
+    }
+}
