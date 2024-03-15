52c94f4b6a493fe68ddcb5ad093a5536c954ae54
long100109 <Ngolong2010@gmail.com>
2024-03-15 11:02:12 +0700

    Chuong 2

--- Baithuchanh/Employee/src/employee1.java
+++
@@ -1,8 +0,0 @@
-public class employee1 {
-    public static void main(String[] args) {
-        Employee employee1= new Employee();
-        employee1.Inputinfo();
-        employee1.Displayinfo();        
-    }
-}
-
--- Baithuchanh/Studentbth/src/student1.java
+++
@@ -1,7 +0,0 @@
-public class student1 {
-    public static void main(String[] args) {
-        Studentbth student = new Studentbth();
-        student.Inputinfo();
-        student.Displayinfo();        
-    }
-}
